66a67718c7df18cd918820de24a1cf5751b00c1c
T0RYeshka <121380862+T0RYTheWorld@users.noreply.github.com>
2024-11-15 21:30:33 +0300

    Update forms.py

--- web_1/hello/firstapp/forms.py
+++ web_1/hello/firstapp/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
-class UserForm(forms.Form):
+class UserForm(forms.ModelForm):
     name = forms.CharField()
     age = forms.IntegerField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'age',]
